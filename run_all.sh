b50e60141d7309c9314c21199bbdab736ee33236
omriel <omri.elmalech@gmail.com>
2022-07-25 20:39:39 +0300

    adding sourch file to execute the python code

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+rm -f nohup.out
+nohup python3 try1.py NFLX &
+nohup python3 try1.py INTC &
+nohup python3 try1.py AAPL &
+nohup python3 try1.py GOOG &
+nohup python3 try1.py AMZN &
+nohup python3 try1.py TSLA &
