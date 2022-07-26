ace1b356c983fba4f9eea06278c8a37f2fe1c92f
omriel <omri.elmalech@gmail.com>
2022-07-26 21:12:50 +0300

    run_all.sh fix

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 rm -f nohup.out
-nohup python3 try1.py NFLX &
-nohup python3 try1.py INTC &
-nohup python3 try1.py AAPL &
-nohup python3 try1.py GOOG &
-nohup python3 try1.py AMZN &
-nohup python3 try1.py TSLA &
+nohup python3 try1_git.py NFLX &
+nohup python3 try1_git.py INTC &
+nohup python3 try1_git.py AAPL &
+nohup python3 try1_git.py GOOG &
+nohup python3 try1_git.py AMZN &
+nohup python3 try1_git.py TSLA &
